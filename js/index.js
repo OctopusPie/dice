6b0b31b67876c9cdb5dfcf39204a56af7384f8e5
Arthur Vadrot <krafteck666@gmail.com>
2022-03-25 03:28:20 +0100

    feat: Added functions to respond to the route /roll
    Added some generic utils function and the parsing.
    Added the roll function to the route POST /roll

--- js/index.js
+++ js/index.js
@@ -1,16 +1,18 @@
 const express = require('express');
+const bp = require('body-parser')
+
 const utils = require('./utils.js');
 const CONFIG = require('./config.json');
 
 const app = express();
+app.use(bp.json())
+app.use(bp.urlencoded({ extended: true }))
 
 app.listen(CONFIG.listeningPort, () => {
-    console.log('Serveur à l\'écoute');
+    console.log('Server is listening');
 });
 
-app.get('/roll', (req,res) => {
-    res.status(200).json({
-        roll: utils.roll(),
-        success: true
-    });
+app.post('/roll', (req,res) => {
+    console.log('POST /roll');
+    res.status(200).json(utils.roll(req.body));
 })
